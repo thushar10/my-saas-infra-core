d0cbca94b2fb059727fb53fb75f13d0a3dbcfa3d
Thushar P Sujir <thushar.gokusujir@gmail.com>
2025-07-10 14:58:05 +0530

    updated - rds secret management

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -7,16 +7,38 @@ data "aws_secretsmanager_secret_version" "db_password" {
   secret_id = "baas-db-password"
 }
 
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
+  description = "Allow DB access from EKS"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_cidr_blocks  # e.g., EKS private subnets
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_db_instance" "baas_rds" {
   identifier         = "${var.project_name}-rds"
   allocated_storage  = var.allocated_storage
   engine             = "postgres"
   instance_class     = var.instance_class
-  name               = var.db_name
+  #name               = var.db_name
   username           = "admin"
   password           = jsondecode(data.aws_secretsmanager_secret_version.db_password.secret_string)["password"]
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-  vpc_security_group_ids = []  # Optional: security groups
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot = true
   publicly_accessible = false
+
 }
--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -9,3 +9,9 @@ variable "vpc_id" {}
 variable "project_name" {
   type = string
 }
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  description = "CIDR blocks allowed to connect to RDS"
+  default     = ["10.0.11.0/24", "10.0.12.0/24"] # your EKS private subnets
+}
